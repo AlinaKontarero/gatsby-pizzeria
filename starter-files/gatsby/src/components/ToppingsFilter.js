96743205396e2d468ff047cf8a469f83a82f7bcb
alina-kontarero <alina.kontarero@gmail.com>
2020-10-31 19:07:24 +1100

    Finished module #7

---
+++ starter-files/gatsby/src/components/ToppingsFilter.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import styled from 'styled-components';
+
+const ToppingStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    .active {
+      background: var(--yellow);
+    }
+    &:hover {
+      background: var(--yellow);
+    }
+  }
+`;
+
+function countPizzasInToppings(pizzas) {
+  const counts = pizzas
+    .map((_pizza) => _pizza.toppings)
+    .flat()
+    // eslint-disable-next-line array-callback-return
+    .reduce((acc, topping) => {
+      const existingTopping = acc[topping.id];
+      if (existingTopping) {
+        existingTopping.count += 1;
+      } else {
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+
+  const sortedToppings = Object.values(counts).sort(
+    (a, b) => b.count - a.count
+  );
+  console.log('sortedToppings::::', sortedToppings);
+  return sortedToppings;
+}
+
+export default function ToppingsFilter() {
+  const { toppings, pizzas } = useStaticQuery(graphql`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          id
+          name
+          vegetarian
+        }
+      }
+      pizzas: allSanityPizza {
+        nodes {
+          toppings {
+            id
+            name
+            vegetarian
+          }
+        }
+      }
+    }
+  `);
+  const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
+  return (
+    <ToppingStyles>
+      {toppingsWithCounts.map((_topping) => (
+        <Link key={_topping.id} to={`topping/${_topping.name}`}>
+          <span className="name">{_topping.name} </span>
+          <span className="count">{_topping.count} </span>
+        </Link>
+      ))}
+    </ToppingStyles>
+  );
+}
--- starter-files/gatsby/src/pages/pizzas.js
+++ starter-files/gatsby/src/pages/pizzas.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import PizzaList from '../components/PizzaList';
+import ToppingsFilter from '../components/ToppingsFilter';
 
 export default function PizzasPage({ data }) {
   const pizzas = data.allSanityPizza.nodes;
 
   return (
     <>
+      <ToppingsFilter />
       <p>Hei! There are {pizzas.length} Pizzas! </p>
       <PizzaList pizzas={pizzas} />
     </>
@@ -27,8 +29,11 @@ export const query = graphql`
         }
         image {
           asset {
-            fluid(maxWidth: 400) {
+            fluid(maxWidth: 300) {
               ...GatsbySanityImageFluid
+            }
+            fixed(width: 200, height: 300) {
+              ...GatsbySanityImageFixed
             }
           }
         }
