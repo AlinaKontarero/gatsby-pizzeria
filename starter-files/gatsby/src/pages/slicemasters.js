ec734736a2b6df99fb7f330378749ff44fddb37d
alina-kontarero <alina.kontarero@gmail.com>
2020-11-09 18:11:31 +1100

    Finished lesson #28

--- starter-files/gatsby/src/pages/slicemasters.js
+++ starter-files/gatsby/src/pages/slicemasters.js
@@ -1,9 +1,84 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function SlicemastersPage() {
+const SliceMasterGrid = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SliceMasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+
+export default function SlicemastersPage({ data }) {
+  const slicemasters = data.slicemasters.nodes;
   return (
-    <div>
-      <p>Hey! This is a Slicemasters Page </p>
-    </div>
+    <>
+      <SliceMasterGrid>
+        {slicemasters.map((person) => (
+          <SliceMasterStyles>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name} </span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} alt={person.name} />
+            <p className="description">{person.description}</p>
+          </SliceMasterStyles>
+        ))}
+      </SliceMasterGrid>
+    </>
   );
 }
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        id
+        name
+        image {
+          asset {
+            fluid(maxWidth: 300) {
+              ...GatsbySanityImageFluid
+            }
+            fixed(width: 200, height: 300) {
+              ...GatsbySanityImageFixed
+            }
+          }
+        }
+        slug {
+          current
+        }
+        description
+      }
+    }
+  }
+`;
