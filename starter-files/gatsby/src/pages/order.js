238839ec9ac4b7ecdb2d5d0eff32819b68d09ba8
alina-kontarero <alina.kontarero@gmail.com>
2020-11-17 15:29:06 +1100

    Half part video

--- starter-files/gatsby/src/pages/order.js
+++ starter-files/gatsby/src/pages/order.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SEO from '../components/SEO';
 
-export default function OrderPage() {
+// 04-01 from module 10 
+  const [name, setName] = useState('')
   return (
     <div>
-      <SEO title={`Order a Pizza`} />
+      <SEO title="Order a Pizza" />
+      <form>
+        <fieldset>
+          <legend>Your info</legend>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" />
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" />
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" />
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+        </fieldset>
+        <fieldset>
+          <legend>Order</legend>
+        </fieldset>
+      </form>
       <p>Hei! I'm the Order Page </p>
     </div>
   );
