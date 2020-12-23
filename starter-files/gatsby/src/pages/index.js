11aceeb39366476524609db94d252d4f251d5a1f
alina-kontarero <alina.kontarero@gmail.com>
2020-12-23 16:34:06 +1100

    Finished #46

--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import useLatestData from '../utils/useLatestData'
+import useLatestData from '../utils/useLatestData';
+import LoadingGrid from '../components/LoadingGrid';
+import { HomePageGrid } from '../styles/Grids';
 
-const CurrentlySlicing = () => (
+const CurrentlySlicing = ({ slicemasters }) => (
   <div>
-    <p>Currently Slicing </p>
+    {!slicemasters && <LoadingGrid count={4} />}
+    {slicemasters && !slicemasters?.length && <p>No one is working now!</p>}
+    {/* <p>Currently Slicing </p> */}
   </div>
 );
 
-const HotSlices = () => (
+const HotSlices = ({ hotSlices }) => (
   <div>
-    <p>Hot Slices </p>
+    {!hotSlices && <LoadingGrid count={4} />}
+    {hotSlices && !hotSlices?.length && (
+      <p>No current slices now. Please order. </p>
+    )}
+    {/* <p>Hot Slices </p> */}
   </div>
 );
 
-export default function HomePage() {
+const HomePage = () => {
   const { slicemasters, hotSlices } = useLatestData();
   return (
     <div className="center">
       <h1>The Best Pizza in the Sutherland Shire!</h1>
       <p>Open 11 am to 11pm every single day</p>
-      <div>
+      <HomePageGrid>
         <CurrentlySlicing slicemasters={slicemasters} />
         <HotSlices hotSlices={hotSlices} />
-      </div>
+      </HomePageGrid>
     </div>
   );
-}
+};
+
+export default HomePage;
